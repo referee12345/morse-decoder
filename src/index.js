5340e533974e495a296aad7361e3ed514ee6220e
referee12345 <38100703+referee12345@users.noreply.github.com>
2020-10-23 19:36:46 +0300

    feat: implement morse-decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,46 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
-}
 
-module.exports = {
-    decode
-}
+
+function decode(expr) {
+    let constr = [],
+      string = "";
+  
+    const array = expr.match(/.{1,10}/g);
+    for (let i of array) {
+      let number = [];
+      if (i === "**********") {
+        number.push("*");
+      }
+      let letter = i.match(/.{1,2}/g);
+      for (let j = 0; j < letter.length; j++) {
+        if (letter[j] === "10") {
+          number.push(".");
+        } else if (letter[j] === "11") {
+          number.push("-");
+        }
+      }
+      constr.push(number.join(""));
+      number = [];
+    }
+    for (let y = 0; y < constr.length; y++) {
+      if (constr[y] === "*") {
+        string += " ";
+      }
+      for (let i in MORSE_TABLE) {
+        if (constr[y] === i) {
+          string += MORSE_TABLE[i];
+        }
+      }
+    }
+  
+    return string;
+  }
+  
+  module.exports = {
+    decode,
+  };
+
+
+
